fc7f974dbef7c49b5cb8ff1acbd647d42faa7d64
minami <reclamationttt@icloud.com>
2020-08-06 03:21:27 +0000

    ユーザ登録機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root to: 'yarukotos#index'
 
+  get 'signup', to: 'users#new'
+  resources :users, only: [:new, :create]
+  
   resources :yarukotos
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_03_052000) do
+ActiveRecord::Schema.define(version: 2020_08_05_085237) do
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "yarukotos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
